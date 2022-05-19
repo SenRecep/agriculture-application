a8aeb919e59d9e7d4e99650ae3ad4442544699b6
Recep Şen <195804015@ogr.cbu.edu.tr>
2022-05-19 12:52:09 +0300

    db changes

--- src/services/webapi/src/typeorm/User.entity.ts
+++ src/services/webapi/src/typeorm/User.entity.ts
@@ -2,13 +2,14 @@ import { Column, Entity, Unique } from 'typeorm';
 import { EntityBase } from './EntityBase.entity';
 
 @Entity({ name: 'users' })
-@Unique(['email'])
+// @Unique(['email'])
 export class User extends EntityBase {
   @Column({ name: 'first_name', nullable: false, default: '' })
   firstName: string;
   @Column({ name: 'last_name', nullable: false, default: '' })
   lastName: string;
-  @Column({ name: 'email', nullable: false, default: '', unique: true })
+  // @Column({ name: 'email', nullable: false, default: '', unique: true })
+  @Column({ name: 'email', nullable: false, default: '' })
   email: string;
   @Column({ name: 'password', nullable: false, default: '' })
   password: string;
